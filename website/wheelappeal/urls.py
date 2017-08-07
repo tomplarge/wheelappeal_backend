65e6f80e80032bea0691fa1b375a44d834621ba6
EC2 Default User <ec2-user@ip-172-31-34-76.us-east-2.compute.internal>
2017-08-07 01:36:03 +0000

    html for form working

--- website/wheelappeal/urls.py
+++ website/wheelappeal/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^api', views.api, name='api'),
+    url(r'^api/', views.api, name='api'),
+    url(r'^submit/', views.submit, name='submit')
 ]
