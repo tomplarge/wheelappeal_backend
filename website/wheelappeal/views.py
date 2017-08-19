f4d31e8f9ec5f1a216b8a366698a3a29c371154f
Tom Large <tomlarge@Toms-MacBook-Pro.local>
2017-08-19 11:35:21 -0400

    more logging

--- website/wheelappeal/views.py
+++ website/wheelappeal/views.py
@@ -18,28 +18,13 @@ def index(request):
     context = {}
     return render(request, 'wheelappeal/index.html', context)
 
+
 def api(request):
+    # TODO: move REST api here
     return HttpResponse('This is our API. It is currently under construction.')
 
-# def submit(request):
-#     # if this is a POST request we need to process the form data
-#     if request.method == 'POST':
-#         form = TruckForm(request.POST)
-#         # check whether it's valid:
-#         if form.is_valid():
-#             print form
-#             return HttpResponseRedirect('/')
-#
-#     # if a GET (or any other method) we'll create a blank form
-#     else:
-#         form = TruckForm()
-#
-#     return render(request, 'wheelappeal/submit.html', {'form': form})
 
 def submit(request):
-    """
-    Allows a user to update their own profile.
-    """
     # Create the formset, specifying the form and formset we want to use.
     MenuFormSet = formset_factory(MenuForm, formset=BaseMenuFormSet)
 
@@ -48,6 +33,7 @@ def submit(request):
         menu_formset = MenuFormSet(request.POST)
 
         if truck_form.is_valid() and menu_formset.is_valid():
+            # TODO: implement form errors for onscreen display
             logging.debug("Menu and truck forms are valid")
             truck_name = truck_form.cleaned_data.get('truck_name')
             cuisine = truck_form.cleaned_data.get('cuisine')
@@ -55,8 +41,13 @@ def submit(request):
             menu_items = []
 
             for menu_form in menu_formset:
-                item_name = menu_form.cleaned_data.get('item_name')
-                item_price = menu_form.cleaned_data.get('item_price')
+                if 'item_name' in menu_form.cleaned_data:
+                    item_name = menu_form.cleaned_data.get('item_name')
+                else:
+                    logging.warning("Field 'item_name' not present in menu_form. Dumping menu_form:")
+
+                if 'item_price' in menu_form.cleaned_data:
+                    item_price = menu_form.cleaned_data.get('item_price')
 
                 if item_name and item_price:
                     menu_items.append({'name':item_name, 'price':item_price})
@@ -75,14 +66,21 @@ def submit(request):
             # except IntegrityError: #If the transaction failed
             #     messages.error(request, 'There was an error saving your profile.')
             #     return redirect(reverse('profile-settings'))
-            truck_data = {'truck_name':truck_name, 'cuisine':cuisine, 'menu': menu_items}
+            truck_data = {'truck_name': truck_name, 'cuisine': cuisine, 'menu': menu_items}
             response = utils.post_truck_data(truck_data)
             if response:
                 return HttpResponse('Successful Post')
             else:
                 logging.debug("Error in truck submission: %s" % (response))
                 return HttpResponse("ERROR!! " + response.text)
+
+        else:
+            # forms are not valid
+            # TODO dump menu and truck form
+            logging.debug("Form not valid. Cleaned data: %s" % (truck_form.cleaned_data))
+            return HttpResponse("There was an issue with your submission. Please try again.")
     else:
+        # GET
         truck_form = TruckForm()
         menu_formset = MenuFormSet()
 
@@ -90,5 +88,5 @@ def submit(request):
         'truck_form': truck_form,
         'menu_formset': menu_formset,
     }
-    
+
     return render(request, 'wheelappeal/submit.html', context)
